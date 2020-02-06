979c45e0f34b33ad932eee0e0835439e68c747f6
kongou <oukong84@gmail.com>
2020-02-06 21:19:40 +0700

    Implemented format number

--- app.js
+++ app.js
@@ -146,6 +146,24 @@ var UIController = (function() {
     expensePercLabel: '.item__percentage'
   };
 
+  var formatNumber = function(num, type) {
+    var numSplit, int, dec, type;
+
+    num = Math.abs(num);
+    num = num.toFixed(2);
+
+    numSplit = num.split('.');
+
+    int = numSplit[0];
+    if (int.length > 3) {
+      int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+    }
+
+    dec = numSplit[1];
+
+    return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+  };
+
   return {
     getInput: function() {
       return {
@@ -174,7 +192,7 @@ var UIController = (function() {
       // Replace the placeholder text with some actual data
       newHtml = html.replace('%id%', obj.id);
       newHtml = newHtml.replace('%description%', obj.description);
-      newHtml = newHtml.replace('%value%', obj.value);
+      newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 
       // Insert the HTMl into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
