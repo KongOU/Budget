fa7598d7898361f931844f25f92dfbe4c5d33a6c
kongou <oukong84@gmail.com>
2020-02-06 22:32:13 +0700

    Complete Budget App

--- app.js
+++ app.js
@@ -165,6 +165,12 @@ var UIController = (function() {
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
   };
 
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function() {
       return {
@@ -230,15 +236,9 @@ var UIController = (function() {
     },
 
     displayPercentages: function(percentages) {
-      var fields, nodeListForEach;
+      var fields;
 
       fields = document.querySelectorAll(DOMStrings.expensePercLabel);
-
-      nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
 
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0) {
@@ -256,8 +256,17 @@ var UIController = (function() {
       months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
       month = now.getMonth();
       year = now.getFullYear();
-      console.log(month, year);
       document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
+
+    changedType: function() {
+      var fields = document.querySelectorAll(DOMStrings.inputType + ',' + DOMStrings.inputDescription + ',' + DOMStrings.inputValue);
+
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMStrings.inputButton).classList.toggle('red');
     },
 
     getDOMStrings: function() {
@@ -280,6 +289,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
   };
 
   var updateBudget = function() {
